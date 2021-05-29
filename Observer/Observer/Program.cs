cf7b4f8ad5eb19b7f4efdad3e3fa26bbd00003be
dma003 <deepumon.manoharan@logstarerp.com>
2021-05-29 14:48:29 +0530

    example now based on component notification

--- Observer/Observer/Program.cs
+++ Observer/Observer/Program.cs
@@ -18,18 +18,7 @@ namespace RefactoringGuru.DesignPatterns.Observer.Conceptual
         public string EventData { get; set; }
 
     }
-
-    public interface ISubject
-    {
-        // Attach an observer to the subject.
-        void Attach(IObserver observer);
-
-        // Detach an observer from the subject.
-        void Detach(IObserver observer);
-
-        // Notify all observers about an event.
-        void Notify();
-    }
+     
 
     // The Subject owns some important state and notifies observers when the
     // state changes.
@@ -63,7 +52,7 @@ namespace RefactoringGuru.DesignPatterns.Observer.Conceptual
 
             foreach (var observer in _observers)
             {
-                //fired
+                //Although all are notified, not every observer will react to this
                 observer.Update(evt);
             }
         }
@@ -72,33 +61,34 @@ namespace RefactoringGuru.DesignPatterns.Observer.Conceptual
         // can really do. Subjects commonly hold some important business logic,
         // that triggers a notification method whenever something important is
         // about to happen (or after it).
-        public void SomeoneClickedSidebar()
+        public void SomeoneClickedSidebar(string e)
         {
              this.State = new Random().Next(0, 10);
 
             Thread.Sleep(15);
-            var evt = new EventInfo() { EventName = "windowOpen", EventData = "vessel:1122" };
+            var evt = new EventInfo() { EventName = "windowOpen", EventData = e };
             this.Notify(evt);
         }
     }
 
     // Concrete Observers react to the updates issued by the Subject they had
     // been attached to.
-    class ConcreteObserverA : IObserver
+    class VesselComponent : IObserver
     {
         public void Update(EventInfo evt)
         {
-
-            Console.WriteLine("ConcreteObserverA: Reacted to the event . Event Name : "+evt.EventName.ToString() + " Event :"+evt.EventData.ToString());
+            if(evt.EventName =="windowOpen" && evt.EventData.Split(':')[0] =="Vessel")
+            Console.WriteLine("VesselComponent: Reacted to the event . Event Name : "+evt.EventName.ToString() + " Event :"+evt.EventData.ToString());
 
         }
     }
 
-    class ConcreteObserverB : IObserver
+    class ContainerComponent : IObserver
     {
         public void Update(EventInfo evt)
         {
-            Console.WriteLine("ConcreteObserverB: Reacted to the event . Event Name : " + evt.EventName.ToString() + " Event :" + evt.EventData.ToString());
+            if (evt.EventName == "windowOpen" && evt.EventData.Split(':')[0] == "Container")
+                Console.WriteLine("ContainerComponent: Reacted to the event . Event Name : " + evt.EventName.ToString() + " Event :" + evt.EventData.ToString());
         }
     }
 
@@ -108,18 +98,18 @@ namespace RefactoringGuru.DesignPatterns.Observer.Conceptual
         {
             // The client code.
             var subject = new Subject();
-            var observerA = new ConcreteObserverA();
+            var observerA = new VesselComponent();
             subject.Attach(observerA);
 
-            var observerB = new ConcreteObserverB();
+            var observerB = new ContainerComponent();
             subject.Attach(observerB);
 
-            subject.SomeoneClickedSidebar();
-            subject.SomeoneClickedSidebar();
+            subject.SomeoneClickedSidebar("Vessel:1222");
+            subject.SomeoneClickedSidebar("Container:1344");
 
             subject.Detach(observerB);
 
-            subject.SomeoneClickedSidebar();
+            subject.SomeoneClickedSidebar("Vessel:4554");
             Console.ReadLine();
         }
     }
